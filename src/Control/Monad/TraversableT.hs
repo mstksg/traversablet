c5525f561aa328ee661de7c65dd8a621c278f529
Justin Le <justin@jle.im>
2015-07-08 01:13:28 -0700

    removed Monad constraint on Alternative instance by using asum instead of join

--- src/Control/Monad/TraversableT.hs
+++ src/Control/Monad/TraversableT.hs
@@ -37,20 +37,23 @@ instance (Monad t, Traversable t, MonadIO m) => MonadIO (TraversableT t m) where
     liftIO = TraversableT . fmap pure . liftIO
 
 instance (Traversable t, MonadError e t, Monad m) => MonadError e (TraversableT t m) where
-    throwError = TraversableT . pure . throwError
+    throwError = TraversableT . return . throwError
     catchError x f = TraversableT $ do
         x' <- runTraversableT x
         let y = sequence $ fmap pure x' `catchError` \e -> pure (f e)
         join <$> runTraversableT y
 
-instance (Alternative t, Traversable t, Monad t, Monad m) => Alternative (TraversableT t m) where
+instance (Alternative t, Traversable t, Monad m) => Alternative (TraversableT t m) where
     empty = TraversableT $ pure empty
     x <|> y = TraversableT $ do
         x' <- runTraversableT x
-        let c = (pure . pure <$> x') <|> pure (runTraversableT y)
-        join <$> sequence c
+        let c = sequence $ (return . pure <$> x') <|> pure (runTraversableT y)
+        -- c :: m (t (t a))
+        -- TODO: is asum okay and not join?
+        -- join <$> c
+        asum <$> c
 
-instance (MonadPlus t, Traversable t, Monad t, Monad m) => MonadPlus (TraversableT t m) where
+instance (MonadPlus t, Traversable t, Monad m) => MonadPlus (TraversableT t m) where
     mzero = empty
     mplus = (<|>)
 
